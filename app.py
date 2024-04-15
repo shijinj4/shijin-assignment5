af24c1cec056abeb755ebadfcb22fd5a3035c1a4
shijinj4 <67093391+shijinj4@users.noreply.github.com>
2024-04-14 22:24:10 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import socket
+ 
+app = Flask(__name__)
+ 
+ 
+hostname = socket.gethostname()
+try:
+    ip_address = socket.gethostbyname(hostname)
+except socket.gaierror:
+    ip_address = 'Unable to resolve IP'
+ 
+@app.route('/')
+def hello_cloud():
+  return "Hello from Shijin joseph ECS Container"
+ 
+@app.route('/host')
+def host_name():
+  return hostname
+ 
+@app.route('/ip')
+def host_ip():
+  return ip_address
+ 
+app.run(host='0.0.0.0')
